c7f371b37b094edf73398ddd26888e46018179fd
Mirko Borivojevic <mirko.borivojevic@gmail.com>
2013-03-12 22:22:28 +0100

    Add configuration that triggers segmentation fault error

---
+++ app/Model/SalesForce.php
@@ -0,0 +1,11 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * SalesForce Model
+ */
+class SalesForce extends AppModel {
+
+    public $useTable = false;
+
+    public $useDbConfig = 'salesforce';
+}
